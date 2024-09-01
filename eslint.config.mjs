8b4d06dbabe9588457932f493b32084b45d4dd0e
librz <rz19960718@gmail.com>
2024-09-02 04:26:27 +0800

    feat: setup eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  { files: ["src/**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      // prevent reassigning/mutating function parameters
+      "no-param-reassign": ["error", { props: true }],
+      // prefer const over let
+      "prefer-const": ["error"],
+    },
+  },
+];
